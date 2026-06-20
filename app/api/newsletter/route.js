2a981f1534c8a27b87c335ee07ccb717931527f3
diegodiezp <diegodiezp@gmail.com>
2026-06-20 16:14:42 +0200

    Update route.js

--- app/api/newsletter/route.js
+++ app/api/newsletter/route.js
@@ -8,11 +8,11 @@ export async function POST(request) {
   }
 
   const fields = { Email: email };
-  if (name)     fields['Name']         = name;
-  if (surname)  fields['Surname']      = surname;
-  if (phone)    fields['Phone Number'] = phone;
+  if (name)     fields['First Name']   = name;
+  if (surname)  fields['Last Name']    = surname;
+  if (phone)    fields['Phone']        = phone;
   if (city)     fields['City']         = city;
-  if (interest) fields['Interest']     = interest;
+  if (interest) fields['Notes']        = interest;
 
   const res = await fetch(
     `https://api.airtable.com/v0/appFkqvnXlu2Y1Fe4/tbl3NlUODD2Ztq3sl`,
